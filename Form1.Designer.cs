4acd5c8d4dc74ad0bb4e5718eddb7e8f1cbda7ec
Paul <paulskirk53@gmail.com>
2022-03-06 18:28:36 +0000

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Visual_Compass
+{
+    partial class Compass_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.Canvas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Control;
+            this.Canvas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Canvas.Controls.Add(this.label4);
+            this.Canvas.Controls.Add(this.label3);
+            this.Canvas.Controls.Add(this.label2);
+            this.Canvas.Controls.Add(this.label1);
+            this.Canvas.Location = new System.Drawing.Point(150, 124);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(255, 239);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "E";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(223, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "W";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "S";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(125, 459);
+            this.hScrollBar1.Maximum = 360;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(334, 28);
+            this.hScrollBar1.SmallChange = 2;
+            this.hScrollBar1.TabIndex = 2;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // Compass_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 524);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.Canvas);
+            this.DoubleBuffered = true;
+            this.Name = "Compass_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Compass";
+            this.Canvas.ResumeLayout(false);
+            this.Canvas.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Visual_Compass
+{
+    public partial class Compass_Form : Form
+    {
+        static Pen myPen = new Pen(Color.Black);
+        
+        Graphics g = null;
+     
+
+
+        static Bitmap baseImage = (Bitmap)Image.FromFile(@"C:\Compass_Images\Compass2.png");  // this is the compass background image
+
+        static Image imgpk = Image.FromFile(@"C:\Compass_Images\Compass2.png");      // alternative image format
+
+        
+        static int start_x = 0;
+        static int start_y = 0;
+        static int end_x  = 0; 
+        static int end_y  = 0;
+        static int length = 55;
+        static int AzimuthOffset = 90;
+        static int Azimuth = 0;
+
+
+  
+        public Compass_Form()
+        {
+            
+            InitializeComponent();
+
+            Canvas.BackgroundImage = baseImage;
+
+
+            start_x = Canvas.Width / 2;       //Canvas is the name of the panel control, this defines the centre point
+            start_y = Canvas.Height / 2;
+
+            //todo remove if not using the picturebox
+           // start_x = pictureBox1.Width / 2;
+            //start_y = pictureBox1.Height / 2;
+          //  Canvas.BackgroundImage = baseImage;
+            
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+
+            
+            g = e.Graphics;                            // uses the Canvas_Paint event handler e instead of creategraphics()
+            myPen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor ;     // pen with arrowhead
+            myPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;          // pen with rounded ends
+            myPen.Width = 1;
+
+           //  g = Canvas.CreateGraphics();            // graphics just for the canvas panel
+            
+            //g = e.Graphics;                            // uses the Canvas_Paint event handler e instead of creategraphics()
+           
+            
+           
+           g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            
+            drawLine();                               // pass the azimuth to this
+            
+            g.Dispose();
+          
+
+        }
+        private void drawLine()    // in the compass / encoder version this will need a parameter - Azimuth
+        {
+            end_x = (int)(start_x + Math.Cos((Azimuth + AzimuthOffset) * 0.017453292519) * length);    // the plus 90 is an offset such that the 
+            end_y = (int)(start_y + Math.Sin((Azimuth + AzimuthOffset) * 0.017453292519) * length);    // pointer points to the correct position for a given Azimuth
+
+            Point[] points =
+            {
+                new Point (start_x, start_y) ,              // note comma
+                new Point (end_x , end_y )                  // 
+            };                                              // note ; after }
+            
+            g.DrawLines(myPen, points);                 // draws the line with the points given
+
+            
+        }  // end drawline
+
+   
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            Canvas.Invalidate();
+         
+            Azimuth = hScrollBar1.Value;
+            //Canvas.Refresh();
+            
+           //Canvas.Invalidate();
+            
+           
+            
+        }
+
+        
+    }
+}
